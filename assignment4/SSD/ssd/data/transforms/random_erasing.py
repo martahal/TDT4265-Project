4ea4780edee9318260b483b7d45a6474d5cff51e
Jesper Paulsen <jesper@jesper.no>
2021-04-22 18:51:52 +0200

    fixed random erasing added contrast

--- assignment4/SSD/ssd/data/transforms/random_erasing.py
+++ assignment4/SSD/ssd/data/transforms/random_erasing.py
@@ -1,16 +1,20 @@
 from __future__ import absolute_import
 
 from torchvision.transforms import *
-
+from transforms import ConvertFromInts, ToAbsoluteCoords
+from contrast import Contrast
 from PIL import Image
 import random
 import math
 import numpy as np
 import torch
+from matplotlib import pyplot as plt
+import cv2
 
 
 # Copied from https://github.com/zhunzhong07/Random-Erasing/blob/master/transforms.py
-# Minor modifications have been applied to make it work with the code
+# Modifications have been applied to make it work with this codebase
+
 
 class RandomErasing(object):
     '''
@@ -24,36 +28,57 @@ class RandomErasing(object):
     -------------------------------------------------------------------------------------
     '''
 
-    def __init__(self, probability=0.5, sl=0.02, sh=0.4, r1=0.3, mean=[0.4914, 0.4822, 0.4465]):
+    def __init__(self, probability=1, sl=0.02, sh=0.08, r1=0.3, mean=[123.675, 116.280, 103.530]):
         self.probability = probability
         self.mean = mean
         self.sl = sl
         self.sh = sh
         self.r1 = r1
 
-    def __call__(self, img, boxes, labels):
+    def __call__(self, img, boxes=None, labels=None):
 
         if random.uniform(0, 1) > self.probability:
             return img, boxes, labels
 
+        print(img.shape[0], img.shape[1])
         for attempt in range(100):
-            area = img.shape[1] * img.shape[2]
+            area = img.shape[0] * img.shape[1]
 
             target_area = random.uniform(self.sl, self.sh) * area
+
             aspect_ratio = random.uniform(self.r1, 1 / self.r1)
 
             h = int(round(math.sqrt(target_area * aspect_ratio)))
             w = int(round(math.sqrt(target_area / aspect_ratio)))
 
-            if w < img.shape[2] and h < img.shape[1]:
-                x1 = random.randint(0, img.shape[1] - h)
-                y1 = random.randint(0, img.shape[2] - w)
-                if img.shape[0] == 3:
-                    img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
-                    img[1, x1:x1 + h, y1:y1 + w] = self.mean[1]
-                    img[2, x1:x1 + h, y1:y1 + w] = self.mean[2]
+            if w < img.shape[0] and h < img.shape[1]:
+                x1 = random.randint(0, img.shape[1] - w)
+                y1 = random.randint(0, img.shape[0] - h)
+                if img.shape[2] == 3:
+                    img[y1:y1 + w, x1:x1 + h, 0] = self.mean[0]
+                    img[y1:y1 + w, x1:x1 + h, 1] = self.mean[1]
+                    img[y1:y1 + w, x1:x1 + h, 2] = self.mean[2]
                 else:
                     img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
                 return img, boxes, labels
 
         return img, boxes, labels
+
+
+def visualize():
+    transform = [
+        Contrast(),
+        ConvertFromInts(),
+        ToAbsoluteCoords(),
+        RandomErasing(),
+    ]
+    # img = cv2.imread('../../../datasets/RDD2020_filtered/JPEGImages/Japan_007344.jpg', 1)
+    img = cv2.imread('Image.png', 1)
+    boxes = None
+    labels = None
+    for t in transform:
+        img, boxes, labels = t(img, boxes, labels)
+
+    cv2.imwrite('erasing.png', img)
+
+visualize()
